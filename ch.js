5c1541f42ee5cfe8c3d5a03c81fee454a337678f
yappedyapper-collab <yappedyapper@gmail.com>
2026-03-27 21:58:05 +0000

    Update ch.js

--- ch.js
+++ ch.js
@@ -1,19 +1,1 @@
-window.game = {
-  addJuice(amount) {
-    juice += amount;
-    update();
-  },
 
-  setJuice(amount) {
-    juice = amount;
-    update();
-  },
-
-  maxAll() {
-    juice = 999999;
-    clickPower = 100;
-    perSecond = 100;
-    rebirths = 10;
-    update();
-  }
-};
